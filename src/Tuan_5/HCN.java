764b4149d60f736e6d783d83b64d2aeb620efd58
ThangMinhLe <minhthangle718@gmail.com>
2023-03-16 16:10:29 +0700

    Update

--- src/Tuan_5/HCN.java
+++ src/Tuan_5/HCN.java
@@ -6,11 +6,36 @@ public class HCN {
     int chieudai;
     int chieurong;
     public HCN (int chieudai, int chieurong) {
-        if(chieudai > 0 && chieurong > 0) {
+        if(chieudai > 0 && chieurong > 0 && chieudai > chieurong) {
         
         this.chieudai = chieudai;
         this.chieurong = chieurong;
     }
+}
+
+public double chuvi () {
+    double c;
+    c = (this.chieudai + this.chieurong) * 2;
+    return c;
+}
+
+
+public double dientich () {
+    double s;
+    s = this.chieudai * this.chieurong;
+    return s;
+}
+
+public void display () {
+    if (chuvi() == 0 || dientich() == 0){
+    System.out.println("Chieu va chieu rong ban nhap khong hop le");
+    
+    }
+    else {
+        System.out.println("Chu vi cua hinh chu nhat la: " + (int) chuvi());
+        System.out.println("Dien tich cua hinh chu nhat la: " + (int) dientich());
+    }
+
 }
 
     public static void main(String[] args) {
@@ -24,30 +49,7 @@ public class HCN {
         HCN hcn1 = new HCN(dai, rong);
         hcn1.display();
     }
-    public double chuvi () {
-        double c;
-        c = (this.chieudai + this.chieurong) * 2;
-        return c;
-    }
-    
 
-    public double dientich () {
-        double s;
-        s = this.chieudai * this.chieurong;
-        return s;
-    }
-
-    public void display () {
-        if (chuvi() == 0 || dientich() == 0){
-        System.out.println("Chieu va chieu rong ban nhap khong hop le");
-        
-        }
-        else {
-            System.out.println("Chu vi cua hinh chu nhat la: " + (int) chuvi());
-            System.out.println("Dien tich cua hinh chu nhat la: " + (int) dientich());
-        }
-    
-    }
 
     // write a function to say hello by java?
     // write a function to take a data by python?
