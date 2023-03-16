5cd8845dc4f4d54026f5a9f411e92e6394eb4418
ThangMinhLe <minhthangle718@gmail.com>
2023-03-16 16:00:55 +0700

    Update Xu Ly HCN

--- src/Tuan_5/HCN.java
+++ src/Tuan_5/HCN.java
@@ -1,15 +1,27 @@
 package Tuan_5;
 
+import java.util.Scanner;
+
 public class HCN {
     int chieudai;
     int chieurong;
     public HCN (int chieudai, int chieurong) {
+        if(chieudai > 0 && chieurong > 0) {
+        
         this.chieudai = chieudai;
         this.chieurong = chieurong;
     }
+}
 
     public static void main(String[] args) {
-        HCN hcn1 = new HCN(3, 4);
+        int dai; int rong;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap chieu dai cho hinh chu nhat: ");
+        dai = sc.nextInt();
+        System.out.print("Nhap chieu rong cho hinh chu nhat: ");
+        rong = sc.nextInt();
+
+        HCN hcn1 = new HCN(dai, rong);
         hcn1.display();
     }
     public double chuvi () {
@@ -26,7 +38,17 @@ public class HCN {
     }
 
     public void display () {
-        System.out.println("Chu vi cua hinh chu nhat la: " + (int) chuvi());
-        System.out.println("Dien tich cua hinh chu nhat la: " + (int) dientich());
+        if (chuvi() == 0 || dientich() == 0){
+        System.out.println("Chieu va chieu rong ban nhap khong hop le");
+        
+        }
+        else {
+            System.out.println("Chu vi cua hinh chu nhat la: " + (int) chuvi());
+            System.out.println("Dien tich cua hinh chu nhat la: " + (int) dientich());
+        }
+    
     }
+
+    // write a function to say hello by java?
+    // write a function to take a data by python?
 }
