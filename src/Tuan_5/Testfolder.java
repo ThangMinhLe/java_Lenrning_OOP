dc884fe64c893e12c68be945ba3efa2a532735a0
ThangMinhLe <minhthangle718@gmail.com>
2023-03-16 08:59:28 +0700

    Hello man add file

---
+++ src/Tuan_5/Testfolder.java
@@ -0,0 +1,14 @@
+package Tuan_5;
+
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Testfolder {
+
+            public static void main(String[] args) {
+                System.out.println("Hello man!");   
+            }
+
+
+    
+}
